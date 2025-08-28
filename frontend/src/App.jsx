e4808781535a6434ca66b22fb99ff21b89f96e04
Bryan <93453907+blopez024@users.noreply.github.com>
2025-08-27 18:52:56 -0700

    restart frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,35 @@
-import { Routes, Route } from 'react-router-dom';
-import {
-  Header,
-  HomePage,
-  AboutPage,
-  AuthPage,
-  AddRecipe,
-  DashboardPage,
-  DashboardHome,
-  MyRecipes,
-  RecipePage,
-  ContactPage,
-} from './components';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-export default function App() {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="recipe/:id" element={<RecipePage />} />
-        <Route path="/contact-us" element={<ContactPage />} />
-
-        {/* Dashboard page w/nested routes */}
-        <Route path="/dashboard" element={<DashboardPage />}>
-          <Route index element={<DashboardHome />} />
-          <Route path="add-recipe" element={<AddRecipe />}></Route>
-          <Route path="my-recipes" element={<MyRecipes />}></Route>
-          <Route path="recipe/:id" element={<RecipePage />}></Route>
-        </Route>
-      </Routes>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
+
+export default App
